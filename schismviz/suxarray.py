aa267b3e6b499e5b8d48a95b0d87e74063f3557a
Kijin Nam <knam@water.ca.gov>
2023-03-13 20:59:47 -0700

    Implement a gr3 reader and triangulate
    The commit add two functions: A function to triangulate the SCHISM
    hybrid grid and another to read the horizontal grid (hgrid) in the
    gr3 file. Corresponding tests are added.
    
    SCH-568

--- schismviz/suxarray.py
+++ schismviz/suxarray.py
@@ -3,7 +3,10 @@
 `suxarray` is a module that extends the functionality of `uxarray` for the
 SCHISM grid.
 """
-
+import numpy as np
+import pandas as pd
+import numba
+import xarray as xr
 import uxarray as ux
 
 
@@ -32,13 +35,22 @@ class Grid(ux.Grid):
         """
         # Add a topology dummy variable if it doesn't exist
         # The current SCHISM out2d does not have this variable.
-        if len(dataset.filter_by_attrs(cf_role="mesh_topology").keys()) == 0:
-            dataset = self.add_SCHISM_mesh_topology_dummy_variable(dataset)
+        if get_topology_variable(dataset) is None:
+            dataset = self.add_topology_variable(dataset)
         # Initialize the super class
         super().__init__(dataset, **kwargs)
 
     @staticmethod
-    def add_SCHISM_mesh_topology_dummy_variable(ds, varname="Mesh2"):
+    def add_topology_variable(ds, varname="SCHISM"):
+        """ Add a dummy mesh_topology variable to a SCHISM out2d dataset
+
+        Parameters
+        ----------
+        ds : xarray.Dataset, required
+            Input SCHISM out2d xarray.Dataset
+        varname : str, optional
+            Name of the dummy topology variable. Default is "Mesh2"
+        """
         ds = ds.assign({varname: 1})
         ds[varname].attrs['cf_role'] = 'mesh_topology'
         ds[varname].attrs['topology_dimension'] = 2
@@ -47,3 +59,120 @@ class Grid(ux.Grid):
         ds[varname].attrs['start_index'] = 1
         ds[varname].attrs['_FillValue'] = -1
         return ds
+
+
+def get_topology_variable(dataset):
+    """ Get the topology xarray.DataArray
+
+    Parameters
+    ----------
+    dataset : xarray.Dataset, required
+        Input xarray.Dataset
+
+    Returns
+    -------
+    da : xarray.DataArray
+        Topology Xarray.DataArray
+    """
+    ds = dataset.filter_by_attrs(cf_role="mesh_topology")
+    if len(ds) == 0:
+        return None
+    elif len(ds) > 1:
+        raise ValueError("Multiple mesh_topology variables found")
+    else:
+        return ds[list(ds.keys())[0]]
+
+
+def triangulate(grid):
+    """ Triangulate a suxarray grid
+
+    Parameters
+    ----------
+    grid : Grid, required
+        Grid object to triangulate
+
+    Returns
+    -------
+    grid : Grid
+        Triangulated grid
+    """
+    mesh_name = grid.ds_var_names['Mesh2']
+    face_nodes = grid.ds[grid.ds_var_names['Mesh2_face_nodes']]
+    n_face, _ = face_nodes.shape
+    fill_value = grid.ds[mesh_name].attrs["_FillValue"]
+    valid = face_nodes != fill_value
+    n_per_row = valid.sum(axis=1)
+    n_triangle_per_row = n_per_row - 2
+    face_ori = np.repeat(np.arange(n_face), n_per_row)
+    node_ori = face_nodes.values.ravel()[valid.values.ravel()]
+
+    def _triangulate(face_ori: np.ndarray, node_ori: np.ndarray,
+                     n_triangle_per_row: xr.DataArray) -> np.ndarray:
+        n_triangle = n_triangle_per_row.sum().item()
+        n_face = len(face_ori)
+        index_first = np.argwhere(np.diff(face_ori, prepend=-1) != 0)
+        index_second = index_first + 1
+        index_last = np.argwhere(np.diff(face_ori, append=-1) != 0)
+
+        first = np.full(n_face, False)
+        first[index_first] = True
+        second = np.full(n_face, True) & ~first
+        second[index_last] = False
+        third = np.full(n_face, True) & ~first
+        third[index_second] = False
+
+        triangles = np.empty((n_triangle, 3), np.int32)
+        triangles[:, 0] = np.repeat(node_ori[first], n_triangle_per_row)
+        triangles[:, 1] = node_ori[second]
+        triangles[:, 2] = node_ori[third]
+        return triangles
+
+    triangles = _triangulate(face_ori, node_ori, n_triangle_per_row)
+
+    triangle_original_ind = np.repeat(
+        np.arange(n_face), repeats=n_triangle_per_row)
+
+    # Copy the data from the original grid
+    ds_tri = grid.ds.copy()
+    # Drop the original face_nodes variable
+    ds_tri = ds_tri.drop_vars(grid.ds_var_names['Mesh2_face_nodes'])
+    da_face_nodes = xr.DataArray(data=triangles,
+                           dims=(f"n{mesh_name}_hgrid_face", "three"),
+                           name=f"{mesh_name}_hgrid_face_nodes")
+    ds_tri[da_face_nodes.name] = da_face_nodes
+    da_elem_ind = xr.DataArray(data=triangle_original_ind,
+                               dims=(f"n{mesh_name}_hgrid_face"),
+                               name=f"{mesh_name}_face_original")
+    ds_tri[da_elem_ind.name] = da_elem_ind
+    grid_tri = ux.Grid(ds_tri, islation=False, mesh_type="ugrid")
+    # grid_tri.Mesh2.attrs['start_index'] = 0
+    return grid_tri
+
+
+def read_hgrid_gr3(path_hgrid):
+    """ Read SCHISM hgrid.gr3 file and return suxarray grid """
+    # read the header
+    with open(path_hgrid, "r") as f:
+        first_line = f.readline()
+        n_faces, n_nodes = [int(x) for x in f.readline().strip().split()[:2]]
+    # Read the node section. Read only up to the fourth column
+    df_nodes = pd.read_csv(path_hgrid, skiprows=2, header=None,
+                           nrows=n_nodes, sep="\s+", usecols=range(4))
+    # Read the face section. Read only up to the sixth column. The last column
+    # may exist or not.
+    df_faces = pd.read_csv(path_hgrid, skiprows=2 + n_nodes, header=None,
+                           nrows=n_faces, sep="\s+", names=range(6))
+    # TODO Read boundary information, if any
+    # Create suxarray grid
+    ds = xr.Dataset()
+    ds['SCHISM_hgrid_node_x'] = xr.DataArray(data=df_nodes[1].values, dims="nSCHISM_hgrid_node")
+    ds['SCHISM_hgrid_node_y'] = xr.DataArray(data=df_nodes[2].values, dims="nSCHISM_hgrid_node")
+    # Replace NaN with -1
+    df_faces = df_faces.fillna(-1)
+    ds['SCHISM_hgrid_face_nodes'] = xr.DataArray(data=df_faces[[2, 3, 4, 5]].values,
+                                                 dims=("nSCHISM_hgrid_face", "nMaxSCHISM_hgrid_face_nodes"))
+    # Add dummy mesh_topology variable
+    ds = Grid.add_topology_variable(ds)
+
+    grid = Grid(ds, islation=False, mesh_type="ugrid")
+    return grid
--- tests/test_suxarray.py
+++ tests/test_suxarray.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import xarray as xr
+import pytest
 import schismviz.suxarray as sx
 
 
@@ -7,7 +8,48 @@ def test_suxarray_init_with_out2d():
     """ Test suxarray initialization with a SCHISM out2d file """
     # Test with a HelloSCHISM v5.10 out2d file
     p_cur = Path(__file__).parent.absolute()
-    ds = xr.open_dataset(str(p_cur / "data/out2d_1.nc"))
+    ds = xr.open_dataset(str(p_cur / "testdata/out2d_1.nc"))
     grid = sx.Grid(ds)
     assert grid.mesh_type == 'ugrid'
     assert grid.ds.dims['nSCHISM_hgrid_node'] == 2639
+
+
+def test_get_topology_variable():
+    """ Test get_topology_variable """
+    # Test with a HelloSCHISM v5.10 out2d file
+    p_cur = Path(__file__).parent.absolute()
+    ds = xr.open_dataset(str(p_cur / "testdata/out2d_1.nc"))
+    da = sx.get_topology_variable(ds)
+    assert da is None
+    # Add a dummy topology variable
+    ds = sx.Grid.add_topology_variable(ds)
+    da = sx.get_topology_variable(ds)
+    assert da.name == 'SCHISM'
+
+
+@pytest.fixture
+def test_grid():
+    """ Test mesh fixture """
+    p_cur = Path(__file__).parent.absolute()
+    grid = sx.read_hgrid_gr3(str(p_cur / "testdata/testmesh.gr3"))
+    return grid
+
+
+def test_triangulate(test_grid):
+    """ Test triangulate """
+    grid_tri = sx.triangulate(test_grid)
+    assert grid_tri.ds.dims['nSCHISM_hgrid_node'] == 112
+    assert grid_tri.ds.dims['nSCHISM_hgrid_face'] == 168
+
+
+def test_read_hgrid_gr3():
+    """ Test read_hgrid_gr3 """
+    # Test with a HelloSCHISM v5.10 hgrid.gr3 file
+    p_cur = Path(__file__).parent.absolute()
+    grid = sx.read_hgrid_gr3(str(p_cur / "testdata/testmesh.gr3"))
+    assert grid.mesh_type == 'ugrid'
+    assert grid.ds.dims['nSCHISM_hgrid_node'] == 112
+    assert grid.ds.dims['nSCHISM_hgrid_face'] == 135
+    # assert grid.ds.dims['nSCHISM_hgrid_edge'] == 10416
+    # assert grid.ds.dims['nSCHISM_hgrid_max_face_nodes'] == 3
+    # assert grid.ds.dims['nSCHISM_hgrid_max_edge_nodes'] == 2
